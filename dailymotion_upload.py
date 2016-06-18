19dcec426c60ce44bccf8c6697da447de72270aa
Sergio Afanou <afanousergio@yahoo.fr>
2016-06-18 16:07:16 +0200

    Catch all the exception in the dailymotion process

--- dailymotion_upload.py
+++ dailymotion_upload.py
@@ -124,61 +124,76 @@ def send_email(sender, recipients, subject, htmlmessage):
 
 def main():
 
-    print "--------------- Starting the dailymotion process --------------- "
-    validextensions = [".mp4", ".mov"]
-    configvars = load_variables("dailymotion.uploader.cfg")
+    errormessage = ""
 
-    API_KEY = configvars['dailymotionkey'].rstrip()
-    API_SECRET = configvars['dailymotionsecretkey'].rstrip()
-    USERNAME = configvars['dailymotion_username'].rstrip()
-    PASSWORD = configvars['dailymotion_password'].rstrip()
-    lockfilename = configvars['lockfilename'].rstrip()
-    sourcepath = configvars['sourcepath'].rstrip()
+    try:
+        print "--------------- Starting the dailymotion process --------------- "
+        validextensions = [".mp4", ".mov"]
+        configvars = load_variables("dailymotion.uploader.cfg")
 
-    # prevent double upload with a lock file
-    check_lock_file(lockfilename)
-    
-    # pick the first video and upload it    
-    print "Processing the source path" , sourcepath
-    nextfiletoprocess = ""
-    for name in os.listdir(sourcepath):    
-        (base, ext) = os.path.splitext(name)
-        if ext in validextensions:
-            print " === Got the one to process : ", os.path.join(sourcepath, name)
-            nextfiletoprocess_name = name
-            nextfiletoprocess_path = os.path.join(sourcepath, name)
+        API_KEY = configvars['dailymotionkey'].rstrip()
+        API_SECRET = configvars['dailymotionsecretkey'].rstrip()
+        USERNAME = configvars['dailymotion_username'].rstrip()
+        PASSWORD = configvars['dailymotion_password'].rstrip()
+        lockfilename = configvars['lockfilename'].rstrip()
+        sourcepath = configvars['sourcepath'].rstrip()
 
-            print "Break out of the loop"
-            break
+        # prevent double upload with a lock file
+        check_lock_file(lockfilename)
+        
+        # pick the first video and upload it    
+        print "Processing the source path" , sourcepath
+        nextfiletoprocess = ""
+        for name in os.listdir(sourcepath):    
+            (base, ext) = os.path.splitext(name)
+            if ext in validextensions:
+                print " === Got the one to process : ", os.path.join(sourcepath, name)
+                nextfiletoprocess_name = name
+                nextfiletoprocess_path = os.path.join(sourcepath, name)
 
-    # Got our file, then upload it
-    nextfiletoprocess_title = "%s auto uploaded" % nextfiletoprocess_name
+                print "Break out of the loop"
+                break
 
-    response = upload(nextfiletoprocess_path, nextfiletoprocess_title, API_KEY, API_SECRET, USERNAME, PASSWORD)
-    #response = "{u'owner': u'xok987', u'id': u'x3kh5zt', u'channel': u'news', u'title': u'PF_video10.mov auto uploaded'}"
+        # Got our file, then upload it
+        nextfiletoprocess_title = "%s auto uploaded" % nextfiletoprocess_name
 
-    # check if the upload is ok
-    #response = response.replace("'", "\"")
-    print response
-    #jsonresponse = json.loads(response)
+        response = upload(nextfiletoprocess_path, nextfiletoprocess_title, API_KEY, API_SECRET, USERNAME, PASSWORD)
+        #response = "{u'owner': u'xok987', u'id': u'x3kh5zt', u'channel': u'news', u'title': u'PF_video10.mov auto uploaded'}"
 
-    if 'id' in response.keys() :
-        print "Upload was successfull. %s found in %s" % (nextfiletoprocess_title, response)
-        # move the file to the archive folder
-        destinationpath = configvars['destination'].rstrip()
-        dst = os.path.join(destinationpath, name)
-        print "Moving video file from %s to %s" % (nextfiletoprocess_path, dst)
-        os.rename(nextfiletoprocess_path, dst)
+        # check if the upload is ok
+        #response = response.replace("'", "\"")
+        print response
+        #jsonresponse = json.loads(response)
 
-    else:
-        print "%s was not found in %s" % (nextfiletoprocess_title, response)
-        print "Upload failed. File will not be moved to the archive so we can process it next time."
+        if 'id' in response.keys() :
+            print "Upload was successfull. %s found in %s" % (nextfiletoprocess_title, response)
+            # move the file to the archive folder
+            destinationpath = configvars['destination'].rstrip()
+            dst = os.path.join(destinationpath, name)
+            print "Moving video file from %s to %s" % (nextfiletoprocess_path, dst)
+            os.rename(nextfiletoprocess_path, dst)
+
+        else:
+            print "%s was not found in %s" % (nextfiletoprocess_title, response)
+            print "Upload failed. File will not be moved to the archive so we can process it next time."
 
 
 
-    # remove the lock file
-    removelockfile(lockfilename)
-    print "--------------- Dailymotion process ended --------------- "
+        # remove the lock file
+        removelockfile(lockfilename)
+        print "--------------- Dailymotion process ended --------------- "
+
+    except:
+        errormessage = errormessage + " -> " + sys.exc_info()[0]
+        logger.debug("Something bad happned. The error is ", sys.exc_info()[0], ". Thats all we know")
+
+    finally:
+
+        dailymotion.send_email('EEPB Video Automator <mailgun@mailgun.bright-softwares.com>',
+            "video@monegliseaparis.fr",
+            "Video file $videofile successfully uploaded to Youtube",
+            "Hello, I have just uploaded the video $videofile to Youtube and I wanted to notify you. Here are the possible errors : " + errormessage + " I am sending this email from the mac computer we use to export videos. I am an Automator application. Enjoy."
+            )
 
 if __name__ == '__main__':
     main()
