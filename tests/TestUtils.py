0eaf6a27fcfab561bc951563c41d778477c32d43
Full Bright <full3right@gmail.com>
2016-11-15 22:18:15 +0100

    Correct unit tests

--- tests/TestUtils.py
+++ tests/TestUtils.py
@@ -7,4 +7,8 @@ class TestUtils(unittest.TestCase):
 		self.assertIsNotNone(variables)
 		assert variables is not None
 
+	def testLoad_variables_fromyaml(self):
+		variables = Utils.load_variables_fromyaml()
+		self.assertIsNotNone(variables)
+		assert variables is not None
 #unittest.main()
