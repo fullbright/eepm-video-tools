7da24366b2fd8216fd87e210da52db90c046e9a0
Full Bright <full3right@gmail.com>
2016-11-15 22:05:00 +0100

    Add unit tests

---
+++ tests/TestUtils.py
@@ -0,0 +1,10 @@
+import unittest
+import Utils
+
+class TestUtils(unittest.TestCase):
+	def testLoad_variables_fromini(self):
+		variables = Utils.load_variables_fromini()
+		self.assertIsNotNone(variables)
+		assert variables is not None
+
+unittest.main()
